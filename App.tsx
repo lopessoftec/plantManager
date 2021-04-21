891a9b5644cfb438a483b999c3880116b2e1eb6a
lopessoftec <lopessoftec@gmail.com>
2021-04-21 00:09:15 -0300

    desenvolvido navegação de tela e criado novas telas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
+// para garantir enquanto as fontes é carregado
+import AppLoading from 'expo-app-loading';
+
+import Routes from './src/routes';
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 export default function App(){
+  // o retorno da primeiro indice do array irei colocar em fontsLoaded
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
   return (
-    <Welcome />
+    <Routes />
   )
 }
